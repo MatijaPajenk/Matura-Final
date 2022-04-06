51d5a1c78c5e5d2598e899ac82226d39f0f35ceb
Matija Pajenk <matija.pajenk@ssravne.si>
2022-04-06 17:52:28 +0200

    commit 6/4/22 - add app logo

--- textas_final/lib/views/signIn.dart
+++ textas_final/lib/views/signIn.dart
@@ -16,30 +16,45 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Center(
-            child: Image.asset(
-              "assets/images/text_only.png",
-              height: 45,
-              color: Theme.of(context).scaffoldBackgroundColor ==
-                      const Color(0xfffec47f)
-                  ? Colors.black
-                  : Colors.white,
-            ),
-          ),
-          toolbarHeight: 70,
+          foregroundColor: Theme.of(context).scaffoldBackgroundColor ==
+                  const Color(0xfffec47f)
+              ? Colors.black
+              : Colors.white,
         ),
         body: Center(
             child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 0),
-          child: ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-                minimumSize: Size(MediaQuery.of(context).size.width / 2, 50)),
-            icon: const FaIcon(FontAwesomeIcons.google),
-            label: const Text('Sign In With Google'),
-            onPressed: () {
-              AuthMethods().signInWithGoogle(context);
-            },
-          ),
-        )));
+                margin: const EdgeInsets.symmetric(horizontal: 0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 128,
+                    ),
+                    Theme.of(context).scaffoldBackgroundColor ==
+                            const Color(0xfffec47f)
+                        ? Image.asset(
+                            'assets/images/logo_in_app.png',
+                            alignment: Alignment.center,
+                            height: 220,
+                          )
+                        : Image.asset(
+                            'assets/images/logo_in_app_dark.png',
+                            alignment: Alignment.center,
+                            height: 220,
+                          ),
+                    const SizedBox(
+                      height: 64,
+                    ),
+                    ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                          minimumSize:
+                              Size(MediaQuery.of(context).size.width / 2, 50)),
+                      icon: const FaIcon(FontAwesomeIcons.google),
+                      label: const Text('Sign In With Google'),
+                      onPressed: () {
+                        AuthMethods().signInWithGoogle(context);
+                      },
+                    ),
+                  ],
+                ))));
   }
 }
